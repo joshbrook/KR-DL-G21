6fc702d11fa3eaed65a6100daa98fed31c11bd63
egetd <ege.t.dinc@gmail.com>
2023-11-21 23:15:51 +0100

    functional reasoner

--- ege_reasoner.py
+++ ege_reasoner.py
@@ -40,7 +40,6 @@ Notes to self:
 - use formatter
 """
 
-
 def check_subsumption(subsumer, subsumee):
     
     # This DICTIONARY will have individuals as keys and their SET of concepts as values
@@ -48,14 +47,12 @@ def check_subsumption(subsumer, subsumee):
 
     # This will keep the dict of roles and successors for each individual
     successor_dict = {"d0" : {}}
-
     changed = True
+    subsumer_found = False
     while changed:
         changed = False
         for d in list(current_interpretation.keys()):
-            changed |= apply_rules(d, current_interpretation, successor_dict)
-            for concept in current_interpretation["d0"]:
-                print()
+            changed |= apply_rules(d, current_interpretation, successor_dict, subsumer, subsumer_found)
 
     # Check if D was assigned to d
     if subsumer in current_interpretation["d0"]:
@@ -63,7 +60,7 @@ def check_subsumption(subsumer, subsumee):
     else:
         return "NO"
     
-def apply_rules(d, current_interpretation, successor_dict):
+def apply_rules(d, current_interpretation, successor_dict, subsumer, subsumer_found):
     changed = False
 
     # ⊤-rule: Add ⊤ to any individual.
@@ -73,22 +70,22 @@ def apply_rules(d, current_interpretation, successor_dict):
         changed = True
 
     # ⊓-rule 1: If d has C ⊓ D assigned, assign also C and D to d.
-##### check if the concept we are adding occurs in the tbox (including nested concepts)
     for concept in list(current_interpretation[d]):
         conceptType = concept.getClass().getSimpleName()
         if conceptType == "ConceptConjunction":
             for conjunct in concept.getConjuncts():
-                    current_interpretation[d].add(conjunct)
-            changed = True
-    print('rule 1 complete')
+                    if conjunct in allConcepts:
+                        current_interpretation[d].add(conjunct)
+                        changed = True
 
     # ⊓-rule 2: If d has C and D assigned, assign also C ⊓ D to d.
     for c1 in list(current_interpretation[d]):
         for c2 in list(current_interpretation[d]):
             if c1 != c2:
-                current_interpretation[d].add(elFactory.getConjunction(c1,c2))
-                changed = True
-    print('rule 2 complete')
+                conjunct_concept = elFactory.getConjunction(c1,c2)
+                if conjunct_concept in allConcepts:
+                    current_interpretation[d].add(conjunct_concept)
+                    changed = True
 
     # ∃-rule 1: If d has ∃r.C assigned, connect to an existing e with C or create one
     found = False
@@ -96,8 +93,8 @@ def apply_rules(d, current_interpretation, successor_dict):
         conceptType = concept.getClass().getSimpleName()
         if conceptType == "ExistentialRoleRestriction":
             # check if there is an existing element with C 
-            for key, value in current_interpretation:
-                if concept.filler() in current_interpretation[key]:
+            for key, value in current_interpretation.items():
+                if formatter.format(concept.filler()) in value:
                     successor_dict[d].add({concept.role : {key}})
                     found = True
                     changed = True
@@ -105,36 +102,48 @@ def apply_rules(d, current_interpretation, successor_dict):
             if not(found):
                 timeID = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
                 current_interpretation[timeID] = {concept.filler()}
-                successor_dict[d].add({concept.role : {timeID}})
+                successor_dict[d][concept.role] = timeID
                 changed = True
-    print('rule 3 complete')
 
     # ∃-rule 2: If d has an r-successor with C assigned, add ∃r.C to d.
-    for role in successor_dict[d].keys():
-        for successor in successor_dict[role]:
-            for concept in current_interpretation[successor]:
-                current_interpretation[d].add(elFactory.getExistentialRoleRestriction(role, concept))
-                changed = True
-    print('rule 4 complete')
+    if d in successor_dict:
+        for role in successor_dict[d]:
+            for successor in successor_dict[d]:
+                if successor in current_interpretation:
+                    for concept in current_interpretation[successor]:
+                        current_interpretation[d].add(elFactory.getExistentialRoleRestriction(role, concept))
+                        changed = True
+
 
     # ⊑-rule: If d has C assigned and C ⊑ D ∈ T, then also assign D to d.
     copy = current_interpretation[d].copy()
     for concept in copy:
-        print('-----' + formatter.format(concept))
         for axiom in tbox:
-            axiomType = axiom.getClass().getSimpleName()
-            if axiomType == "GeneralConceptInclusion" and axiom.lhs() == concept:
-                current_interpretation[d].add(axiom.rhs())
-                changed = True
-    print('rule 5 complete')
+            #print(axiom)
+            if axiom.getClass().getSimpleName() == "GeneralConceptInclusion":
+                left = axiom.lhs()
+                right = axiom.rhs()
+                if left == concept and right in allConcepts and right not in current_interpretation[d]:
+                    current_interpretation[d].add(right)
+                    changed = True
 
+
+    print('____', len(current_interpretation))
+    # for key in current_interpretation.keys():
+    #     for concept in current_interpretation[key]:
+    #         print(formatter.format(concept))
+
+    for concept in current_interpretation["d0"]:
+        if formatter.format(concept) == formatter.format(subsumer):
+            changed = False
+            break
     return changed
 
  
 
  
-input_subsumer = '"Meat"'
-input_subsumee = '"Beef"'
+input_subsumer = "VeganFood"
+input_subsumee = "Salad"
 
 subsumer_concept = elFactory.getConceptName(input_subsumer)
 subsumee_concept = elFactory.getConceptName(input_subsumee)
